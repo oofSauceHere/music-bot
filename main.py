fa6fedebb6d03206d26398c07a0e9dba05e770ba
Deccan Maniam <dev.oofsauce@gmail.com>
2024-11-10 19:55:22 -0800

    Added comments

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class MusicBot(commands.Bot):
+    # constructor
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix=command_prefix, intents=intents)
         self.remove_command("help")
@@ -19,6 +20,7 @@ class MusicBot(commands.Bot):
         self.vid_queue = deque()
         self.loop_vid = False
 
+    # bot starts
     async def on_ready(self):
         print(f"Logged in as {self.user}.")
         await self.change_presence(activity=discord.Game(name="Bloons TD 6"))
@@ -40,7 +42,7 @@ class MusicBot(commands.Bot):
         async def help(ctx):
             await ctx.send("No")
 
-        # joins user's call and plays song from given youtube link
+        # join user's call and play video from given YouTube link (or whatever's in the queue)
         @self.command(name="play")
         async def play(ctx, link=None):
             # doesn't play if user is not in a voice channel
@@ -49,8 +51,9 @@ class MusicBot(commands.Bot):
                 await ctx.send("You need to be in a voice channel.")
                 return
             
-            # oh my goodness
+            # no link specified
             if(link == None):
+                # if in vc, resume playback, else try and play from queue
                 if(self.vc != None):
                     if(self.vc.is_paused()):
                         await ctx.send("Resuming playback.")
@@ -63,11 +66,13 @@ class MusicBot(commands.Bot):
                     await ctx.send("Queue is empty/nothing currently playing.")
                     return
             else:
+                # attempt to access YouTube link
                 try:
                     yt = YouTube(link)
                 except VideoUnavailable:
                     return
                 
+                # if in vc, inform user that video is queued up
                 if(self.vc != None):
                     await ctx.send(f"Added **{yt.title}** to queue.")
                     self.vid_queue.append(yt)
@@ -77,37 +82,46 @@ class MusicBot(commands.Bot):
             
             # connects to voice channel and idles while audio plays
             self.vc = await voice.channel.connect()
-            prev_audio = None
+            prev_audio = None # so looping works efficiently, without the need to re-acquire buffer
             while((self.vid_queue or self.loop_vid) and self.vc != None):
                 audio_buffer = None
+
+                # if we're looping, make sure to line up same video
                 if(self.loop_vid and not prev_audio == None):
                     audio_buffer = prev_audio
                     audio_buffer.seek(0)
                 else:
+                    # remove front video from queue
                     yt = self.vid_queue[0]
                     self.vid_queue.popleft()
                     
+                    # acquire byte buffer from video
                     audio = yt.streams.filter(only_audio=True).first()
                     audio_buffer = io.BytesIO()
                     prev_audio = audio_buffer
                     audio.stream_to_buffer(audio_buffer)
                     audio_buffer.seek(0)
                     # audio.download(filename="temp", mp3=True)
-                    # await ctx.send(link)
 
-                await ctx.send(f"Currently playing **{yt.title}**")
+                # play audio in vc
+                await ctx.send(f"Currently playing **{yt.title}**.")
                 self.vc.play(discord.FFmpegPCMAudio(source=audio_buffer, pipe=True)) # should we define "after" parameter?
 
+                # busy loop until song finishes
                 while(self.vc.is_playing() or self.vc.is_paused()):
                     await asyncio.sleep(0.1)
+            
+            # leave vc
             await self.vc.disconnect()
             self.vc = None
 
+        # toggle loop variable
         @self.command(name="loop")
         async def loop(ctx):
             self.loop_vid = not self.loop_vid
             await ctx.send(f"Loop set to **{self.loop_vid}**")
         
+        # print queue
         @self.command(name="queue")
         async def queue(ctx):
             output = ""
@@ -115,10 +129,11 @@ class MusicBot(commands.Bot):
                 output = output + f"**{i+1}** - {yt.title}\n"
             
             if(self.vid_queue):
-                await ctx.send(output[:-1])
+                await ctx.send(output[:-1]) # strip final newline
             else:
                 await ctx.send("Queue is empty.")
         
+        # add video to queue
         @self.command(name="add")
         async def add(ctx, link=None):
             if(link == None):
@@ -133,15 +148,17 @@ class MusicBot(commands.Bot):
             await ctx.send(f"Added **{yt.title}** to queue.")
             self.vid_queue.append(yt)
         
+        # remove video from queue (referenced via index)
         @self.command(name="delete")
         async def delete(ctx, index=1):
             if(not self.vid_queue):
                 await ctx.send("Queue is empty.")
                 return
             
+            # bounds checking
             if(index <= len(self.vid_queue) and index > 0):
                 await ctx.send(f"Removed **{self.vid_queue[index-1].title}** from queue.")
-                del self.vid_queue[index-1]
+                del self.vid_queue[index-1] # delete from deque via index
             else:
                 await ctx.send("Invalid index.")
         
@@ -151,6 +168,7 @@ class MusicBot(commands.Bot):
                 await ctx.send("Not currently in a call.")
                 return
             
+            # can simply stop playback - current play() loop will detect and continue
             self.vc.stop()
         
         @self.command(name="pause")
@@ -192,10 +210,10 @@ if __name__ == "__main__":
     main()
 
 # TO-DOs
-#   > playlist/queue (differentiate between servers?)
+#   > playlists (differentiate between servers?)
 #       > maybe server-wide playlists?
 #   > is it worth mapping links to byte buffers or will that be too space inefficient?
 #   > maybe add function to convert spotify song title to youtube link?
 #       > much more involved and possibly flawed...
-#   > volume control?
-#   > skip/timestamps
+#   > timestamps
+#   > check if user running command is in vc w/ bot
